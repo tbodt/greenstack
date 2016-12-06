78bae286a5f72fd1883d91adc474e207a8dcc15c
Theodore Dubois <tblodt@icloud.com>
2016-12-05 18:47:06 -0800

    Add a benchmark that switches between to greenlets, to not measure creation time

---
+++ benchmarks/bounce.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+"""Create two greenlets and switch back and forth between them a large number
+of times.
+"""
+
+import optparse
+import time
+
+import greenstack
+
+def switcher(n):
+    while True:
+        if greenstack.getcurrent() == switcher1:
+            n = switcher2.switch(n)
+        else:
+            n = switcher1.switch(n)
+        n -= 1
+        if n == 0:
+            return
+
+if __name__ == '__main__':
+    p = optparse.OptionParser(
+        usage='%prog [-n NUM_BOUNCES]', description=__doc__)
+    p.add_option(
+        '-n', type='int', dest='num_bounces', default=1000000,
+        help='The number of times to switch between the greenlets')
+    options, args = p.parse_args()
+
+    if len(args) != 0:
+        p.error('unexpected arguments: %s' % ', '.join(args))
+
+    start_time = time.clock()
+    switcher1 = greenstack.greenlet(switcher)
+    switcher2 = greenstack.greenlet(switcher)
+    switcher1.switch(options.num_bounces)
+    print time.clock() - start_time, 'seconds'
